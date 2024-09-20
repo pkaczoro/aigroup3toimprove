0588081cb6bdba10930839ea8fea581adceb14d2
pakaczor <pkaczoro@gmail.com>
2024-09-20 11:57:13 +0200

    [aihandson] Generate unit tests, refactor classes

--- src/main/java/org/example/PetStore.java
+++ src/main/java/org/example/PetStore.java
@@ -5,3 +5,10 @@ import java.util.List;
 public interface PetStore {
     public List<String> petsAvailable();
 }
+
+class MyPetStore implements PetStore {
+    @Override
+    public List<String> petsAvailable() {
+        return List.of("Fluffy", "Spot", "Whiskers", "Max", "Bella");
+    }
+} 
