a2ea2862d97fcd364119e2d8e9db64637c5f852d
pakaczor <pkaczoro@gmail.com>
2024-09-20 10:52:48 +0200

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    public void anything() {
+
+    }
+}
---
+++ src/test/java/org/example/model/BallotTest.java
@@ -0,0 +1,17 @@
+package org.example.model;
+
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MainTest {
+
+    @Test
+    void test() {
+            Main m = new Main();
+            m.anything();
+
+    }
+}
